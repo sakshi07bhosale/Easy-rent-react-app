daec868f0ed0a4d43b5291ca5c018350f0eb1a59
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-18 21:31:36 +0530

    property card created

---
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -0,0 +1,6 @@
+import "./PropertyCard";
+function PropertyCard(){
+    return <div>Property Card</div>;
+}
+
+export default PropertyCard;
--- src/views/Properties/Properties.jsx
+++ src/views/Properties/Properties.jsx
@@ -1,5 +1,22 @@
+import "./Properties";
+import {PROPERTIES} from "./../data";
+
 function Properties(){
-    return <div>Properties</div>;
+    return (
+        <div>
+            <h1>Properties</h1>
+            {PROPERTIES.map((propertyObj) => {
+                return(
+                    <div className="property" key={propertyObj.id}>
+                        <h2>{propertyObj.title}</h2>
+                        <p>{propertyObj.description}</p>
+                    </div>
+                );
+            })}
+
+        </div>
+
+    );
 }
 
 export default Properties;
