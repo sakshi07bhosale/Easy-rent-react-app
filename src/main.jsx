88ec4d50842ab37ce7becab75e2600c55d29d433
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-18 17:50:15 +0530

    Added 2 buttons

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import toast, { Toaster } from 'react-hot-toast';
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-createRoot(document.getElementById('root'))
+ const root = createRoot(document.getElementById('root'))
 
 root.render(
     <>
     <h1>Hot Toast</h1>
+    <button onclick={() => {
+        toast.loading('This is a loading toast!');
+    }}>
+        Start loading
+    </button>
+    <button onclick={() => {
+        toast.dismiss();
+    }}>
+         Stop Loading
+    </button>
+    <Toaster />
     </>
 )
