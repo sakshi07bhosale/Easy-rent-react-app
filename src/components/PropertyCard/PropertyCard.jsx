f4fb5f7e9556fd8689b6067a2f69f401fab8498a
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-19 22:22:57 +0530

    property img added

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -1,7 +1,10 @@
 import "./PropertyCard.css";
+import DummyImg from "./../../components/PropertyCard/building.png";
+
 function PropertyCard({ title,  city, area, propertyType }){
     return (
         <div className="property-card">
+            <img src={DummyImg} alt="property" className="property-Img" />
             <h2>{title}</h2>
             <p>Type: {propertyType}</p>
             <p>Area: {area}</p>
