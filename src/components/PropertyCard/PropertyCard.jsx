4764658aa2e451340e12621a2d07f9ef47e051d1
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-19 21:34:23 +0530

    Property card design updated

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -3,7 +3,7 @@ function PropertyCard({ title, description, city, area }){
     return (
         <div className="property-card">
             <h2>{title}</h2>
-            <p>Description: {description}</p>
+            <p>Type: {propertyType}</p>
             <p>Area: {area} </p>
             <p>City: {city}</p>
         </div>
