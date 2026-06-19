e3c90f408583141472c3da52cc5952db9a2d7bae
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-19 22:31:37 +0530

    property title are set perfectly

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -5,7 +5,7 @@ function PropertyCard({ title,  city, area, propertyType }){
     return (
         <div className="property-card">
             <img src={DummyImg} alt="property" className="property-Img" />
-            <h2>{title}</h2>
+            <h2 className="property-title">{title}</h2>
             <p>Type: {propertyType}</p>
             <p>Area: {area}</p>
             <p>City: {city}</p>
