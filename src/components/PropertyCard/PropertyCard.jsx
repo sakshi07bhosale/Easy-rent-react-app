5fb4b7bf6d54e1bf3d7922b7e6e27ac05d97e7bc
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-20 17:41:17 +0530

    separate function are created for same obj

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -2,17 +2,28 @@ import "./PropertyCard.css";
 import DummyImg from "./../../components/PropertyCard/building.png";
 import { ChartArea,Building2,HandCoins,Sofa } from 'lucide-react';
 
+function PropertyInfo({icon, label}) {
+    return (
+        <div className="property-info">
+            {icon} {label}
+        </div>
+    )
+
+}
+
 function PropertyCard({ title, address, city, area, propertyType, rent, rating, furnished }){
     return (
         <div className="property-card">
             <img src={DummyImg} alt="property" className="property-Img" />
             <h2 className="property-title">{title}</h2>
             <p className="property-address">Address: {address}</p>
-            <p><Building2 /> {propertyType}</p>
             <p className="rating-info"> {rating}</p>
-            <p><ChartArea /> {area}</p>
-            <p><HandCoins /> ₹{rent}</p>
-            <p><Sofa /> {furnished ? "Yes" : "No"}</p>
+
+            <PropertyInfo icon={<Building2 />} label={propertyType} />
+            <PropertyInfo icon={<ChartArea />} label={area} />
+            <PropertyInfo icon={<HandCoins />} label={`₹${rent}`} />
+            <PropertyInfo icon={<Sofa />} label={furnished ? "Yes" : "No"} />
+
         </div>
     );
 } 
