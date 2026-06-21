b1b9a220efafe3b6756c7b06efdb188299909e52
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-21 19:45:08 +0530

    properties more details are added

--- src/views/PropertyDetails/PropertyDetails.jsx
+++ src/views/PropertyDetails/PropertyDetails.jsx
@@ -19,6 +19,17 @@ function PropertyDetails() {
     <div>
       <img src={DummuyImg} alt="property" className="property-Img" />
       <h1>{propertyDetails.title ? propertyDetails.title : `Property ${id}`} {id}</h1>
+      <p>{propertyDetails.address}</p>
+      <p>{propertyDetails.propertyType}</p>
+      <p>{propertyDetails.size}</p>
+      <p>{propertyDetails.city}</p>
+      <p>{propertyDetails.area}</p>
+      <p>{propertyDetails.rent}</p>
+      <p>{propertyDetails.furnished ? "Furnished" : "Unfurnished"}</p>
+      <p>Amenities: {propertyDetails.amenities ? propertyDetails.amenities.join(", ") : "N/A"}</p>
+      <p>Rating: {propertyDetails.rating}</p>
+      <p>Reviews: {propertyDetails.reviews}</p>
+      <p>Owner: {propertyDetails.owner ? propertyDetails.owner.name : "N/A"}</p>
     </div>
   )
 }
