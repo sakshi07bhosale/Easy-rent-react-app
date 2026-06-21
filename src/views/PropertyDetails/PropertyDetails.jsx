fadfcd634ce697592908bce5c2e75f8c3fa0a682
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-21 19:21:07 +0530

    dummuy img added

--- src/views/PropertyDetails/PropertyDetails.jsx
+++ src/views/PropertyDetails/PropertyDetails.jsx
@@ -1,12 +1,23 @@
 import "./PropertyDetails.css"
 import { useParams } from "react-router";
 import { useState } from "react";
+import DummuyImg from "./building.png";
+import {PROPERTIES} from "./../data"
 
 function PropertyDetails() {
   const { id } = useParams();
   const [propertyDetails, setPropertyDetails] = useState({});
+
+  useEffect(() => {
+    const property = PROPERTIES.find((prop) => prop.id === parseInt(id));
+    if (property) {
+      setPropertyDetails(property);
+    }
+  }, [id]);
+
   return (
     <div>
+      <img src={DummuyImg} alt="property" className="property-Img" />
       <h1>Property details: { id } </h1>
     </div>
   )
