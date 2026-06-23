540dd060a4e6bec100c8d94a722aae06e4171ff5
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-06-23 21:28:07 +0530

    serachbar created

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,20 @@
-import Input from "./Input";
-function Input(){
-    return <div>Input</div>;
+import "./Input.css";
+function Input(
+    type = "text",
+    placeholder = "Search for properties",
+    value = "",
+    onChange = () => {}
+){
+    return (<div>
+        <input
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className="input-field"
+        />
+    </div>
+    );
 }
 
 export default Input;
--- src/views/Properties/Properties.jsx
+++ src/views/Properties/Properties.jsx
@@ -2,14 +2,16 @@ import PropertyCard from "./../../components/PropertyCard/PropertyCard";
 import { PROPERTIES } from "./../data";
 import "./Properties.css";
 import { useState } from "react";
+import Input from "./../../components/Input/Input";
 
 function Properties() {
     const [properties, setProperties] = useState(PROPERTIES);
     return (
         <div>
             <h1>Properties</h1>
+            <Input />
             <div className="properties-container">
-                {PROPERTIES.map((propertyObj) => {
+                {properties.map((propertyObj) => {
                     const { id, title,  area, propertyType, rent, address, rating,furnished, photos } = propertyObj;
                     return (
                         <PropertyCard
